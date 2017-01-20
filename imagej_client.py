b23c1e998b17fceb31e48c5d897e58394167cbba
Leon Yang <leon.gh.yang@gmail.com>
2017-01-20 15:09:38 -0600

    Add implementation of a basic client

--- imagej_client.py
+++ imagej_client.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python2
 
-"""Interactive client for imagej-server."""
-
 from imagej_server import *
 import argparse
 import cmd
@@ -9,7 +7,105 @@ import json
 import os
 import re
 import shlex
-import sys
+
+
+class Client(object):
+
+    """Basic client for imagej-server."""
+
+    def __init__(self, host=HOST):
+        self.host = HOST
+        self._modules = None
+
+    def modules(self, refresh=False):
+        """Gets the module IDs of imagej-server if no cache is available or
+        refresh is set to True, or returns the cache for the IDs otherwise.
+
+        :param refresh: force fetching modules from imagej-server if True
+        :return: imagej-server module IDs
+        :rtype: list[string]
+        """
+
+        if self._modules is None or refresh:
+            self._modules = get_modules(self.host)
+        return self._modules
+
+    def find(self, regex):
+        """Finds all module IDs that match the regular expression.
+
+        :param regex: the regular express to match the module IDs
+        :return: all matching IDs
+        :rtype: list[string]
+        """
+
+        pattern = re.compile(regex)
+        return filter(pattern.search, self.modules())
+
+    def detail(self, id):
+        """Gets the detail of a module specified by the ID.
+
+        :param id: the ID of the module
+        :return: details of a module
+        :rtype: dict
+        """
+
+        return get_module(id, self.host)
+
+    def run(self, id, inputs=None, process=True):
+        """Runs a module specified by the ID with inputs.
+
+        :param id: the ID of the module
+        :param inputs: a dict-like object containing inputs for the execution
+        :param process: if the execution should be pre/post processed
+        :return: outputs of the execution
+        :rtype: dict
+        """
+
+        return run_module(id, inputs, process, self.host)
+
+    def upload(self, filename):
+        """Uploads a file to imagej-server
+
+        :param filename: filename of the file to be uploaded
+        :return: object ID of the uploaded file
+        :rtype: string
+        """
+
+        with open(filename, 'rb') as data:
+            return upload_file(data, self.host)['id']
+
+    def retrieve(self, source, format=None, config=None, dest=None):
+        """Retrieves the content of an object/file from imagej-server.
+
+        If format is set, source will be interpreted as an object ID, otherwise
+        source should be a file being served on imagej-server and config will be
+        ignored. If dest is None, the raw content would be returned.
+
+        :param source: object ID if format is set, or a file being served
+        :param format: file format the object to be saved into
+        :param config: configuration for storing the object (not tested)
+        :param dest: download destination
+        """
+
+        if format is not None:
+            filename = request_file(
+                source, format, config, self.host)['filename']
+        else:
+            filename = source
+
+        content = retrieve_file(filename, host=self.host)
+        if dest is None:
+            return content
+        if os.path.isdir(dest):
+            dest = os.path.join(dest, filename)
+        else:
+            dir = os.path.dirname(dest)
+            if not os.path.isdir(dir):
+                raise Exception('Directory does not exist: %s' % dir)
+        if os.path.isfile(dest):
+            print('Overwriting existed file: %s' % dest)
+        with open(dest, 'wb') as f:
+            f.write(content)
 
 
 class InteractiveParser(argparse.ArgumentParser):
@@ -28,6 +124,8 @@ class InteractiveParser(argparse.ArgumentParser):
 
 
 class InteractiveClient(cmd.Cmd):
+
+    """Interactive client for imagej-server."""
 
     COMMANDS = frozenset(['list', 'detail', 'iterate', 'run', 'upload',
                           'download'])
