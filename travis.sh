a8b6d6b91105210f5c2586f9d377536e88395d14
kkangle <liu574@wisc.edu>
2018-04-29 22:10:41 -0700

    modified travis and script files to pass travis build

--- travis.sh
+++ travis.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 sudo apt-get update
-if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
 
 conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
 source activate test-environment
 pip install Cython
 pip install pyjnius
 
-sudo apt install curl
-sudo apt install git
+sudo apt -y install curl
+sudo apt -y install git
+sudo apt -y install unzip
 
 cd $HOME
 
@@ -54,11 +44,11 @@ cache:
 conda install -c hanslovsky imglib2-imglyb
 
 git clone https://github.com/kkangle/imagej.py.git
+cd $HOME/imagej.py
 git checkout pyjnius
 
 ij_dir=$HOME/Fiji.app
 echo $ij_dir
-cd $HOME/imagej.py
 python setup.py install
 cd test
 python -O test_imagej.py --ij $ij_dir
