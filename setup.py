548eee03adfca1f4836cc5ebafb877b9a149b539
Curtis Rueden <ctrueden@wisc.edu>
2018-11-29 12:29:31 -0600

    Rename the project: imagej -> pyimagej
    See discussion here:
    https://github.com/conda-forge/staged-recipes/pull/7067#discussion_r235649231
    
    The Python package will still be called imagej. But it
    will now be published as pyimagej on PyPI and conda-forge.

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md')) as f:
-    imagej_long_description = f.read()
+    pyimagej_long_description = f.read()
 
 
 setup(
-    name='imagej',
+    name='pyimagej',
     # TODO: Unify version declaration to one place.
     # https://www.python.org/dev/peps/pep-0396/#deriving
     version= '0.4.0.dev0',
     author='Curtis Rueden, Yang Liu, Leon Yang',
     author_email='ctrueden@wisc.edu',
-    url='https://github.com/imagej/imagej.py',
+    url='https://github.com/imagej/pyimagej',
     packages=['imagej'],
     platforms=['any'],
     description='Python wrapper for ImageJ',
-    long_description=imagej_long_description,
+    long_description=pyimagej_long_description,
     long_description_content_type='text/markdown',
     license='Apache 2.0',
     install_requires=['pyjnius', 'scyjava', 'imglyb']
