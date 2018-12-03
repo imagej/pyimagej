imagej/pyimagej

commit 18bff0c54415079b3771d15741c4eeda31edd2bb

Add ability to spin up ImageJ without local copy
The scyjava library lets us add more endpoints to the classpath,
retrieved from remote Maven repositories, before spinning up Java.
In this way, we can download ImageJ on demand.

If a version is given, add endpoint net.imagej:imagej:<version>.
If an endpoint is given, add that endpoint before creating the gateway.

The unit tests are also updated to support testing ImageJ from Maven
when a local installation directory is not provided.

Closes #9.

imagej/imagej.py
@@ -55,28 +55,50 @@ def set_ij_env(ij_dir):
     # search plugins directory
     jars.extend(search_for_jars(ij_dir, '/plugins'))
     # add to classpath
-    num_jars = len(jars)
     scyjava_config.add_classpath(os.pathsep.join(jars))
-    return num_jars
+    return len(jars)
 
 
-def init(ij_dir, headless=True):
+def init(ij_dir_or_version_or_endpoint=None, headless=True):
     """
     Initialize the ImageJ environment.
 
-    :param ij_dir: System path for local ImageJ installation (e.g. Fiji.app)
-    :param headless: Whether to start the JVM in headless or gui mode
+    :param ij_dir_or_version_or_endpoint:
+        Path to a local ImageJ installation (e.g. /Applications/Fiji.app),
+        OR version of net.imagej:imagej artifact to launch (e.g. 2.0.0-rc-67),
+        OR endpoint of another artifact (e.g. sc.fiji:fiji) that uses imagej.
+    :param headless: Whether to start the JVM in headless or gui mode.
     :return: an instance of the net.imagej.ImageJ gateway
     """
 
     if headless:
         scyjava_config.add_options('-Djava.awt.headless=true')
 
-    plugins_dir = str(Path(ij_dir, 'plugins'))
-    scyjava_config.add_options('-Dplugins.dir=' + plugins_dir)
+    if ij_dir_or_version_or_endpoint is None:
+        # Use latest release of ImageJ.
+        _debug('Using newest ImageJ release')
+        scyjava_config.add_endpoints('net.imagej:imagej')
 
-    num_jars = set_ij_env(ij_dir)
-    print("Added " + str(num_jars + 1) + " JARs to the Java classpath.")
+    elif os.path.isdir(ij_dir_or_version_or_endpoint):
+        # Assume path to local ImageJ installation.
+        path = ij_dir_or_version_or_endpoint
+        _debug('Local path to ImageJ installation given: ' + path)
+        num_jars = set_ij_env(path)
+        print("Added " + str(num_jars + 1) + " JARs to the Java classpath.")
+        plugins_dir = str(Path(ij_dir, 'plugins'))
+        scyjava_config.add_options('-Dplugins.dir=' + plugins_dir)
+
+    elif ':' in ij_dir_or_version_or_endpoint:
+        # Assume endpoint of an artifact.
+        endpoint = ij_dir_or_version_or_endpoint
+        _debug('Maven coordinate given: ' + endpoint)
+        scyjava_config.add_endpoints(endpoint)
+
+    else:
+        # Assume version of net.imagej:imagej.
+        version = ij_dir_or_version_or_endpoint
+        _debug('ImageJ version given: ' + version)
+        scyjava_config.add_endpoints('net.imagej:imagej:' + version)
 
     # Must import imglyb (not scyjava) to spin up the JVM now.
     import imglyb

test/test_imagej.py
@@ -12,7 +12,7 @@ if "--ij" in sys.argv:
     ij = imagej.init(ij_dir)
     sys.argv = sys.argv[2:]
 else:
-    ij_dir = '/Applications/Fiji.app'
+    ij_dir = None # Use newest release version, downloaded from Maven.
     ij = imagej.init(ij_dir)
 
 
@@ -82,6 +82,10 @@ class TestImageJ(unittest.TestCase):
         self.assertEqual(result, correct_result)
 
     def testPluginsLoadUsingPairwiseStitching(self):
+        if ij_dir is None:
+            # HACK: Skip test if not testing with a local Fiji.app.
+            return
+
         macro = """
 
         newImage("Tile1", "8-bit random", 512, 512, 1);

commit 83629152c9501334a70a556ab84d4bda8691e71d

Fix bug in local ImageJ initialization
It was introduced in 18bff0c54415079b3771d15741c4eeda31edd2bb
with the rename of the ij_dir variable to be more inclusive.

imagej/imagej.py
@@ -85,7 +85,7 @@ def init(ij_dir_or_version_or_endpoint=None, headless=True):
         _debug('Local path to ImageJ installation given: ' + path)
         num_jars = set_ij_env(path)
         print("Added " + str(num_jars + 1) + " JARs to the Java classpath.")
-        plugins_dir = str(Path(ij_dir, 'plugins'))
+        plugins_dir = str(Path(path, 'plugins'))
         scyjava_config.add_options('-Dplugins.dir=' + plugins_dir)
 
     elif ':' in ij_dir_or_version_or_endpoint: