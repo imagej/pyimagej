092ab0d4379ca9d4a25b9cf5a481b16610d4ac24
Curtis Rueden <ctrueden@wisc.edu>
2018-12-15 19:01:01 -0600

    Travis: make leading whitespace consistent

--- .travis/build.sh
+++ .travis/build.sh
@@ -28,22 +28,22 @@ cd
 # -- download Fiji.app --
 if [ ! -d Fiji.app ]
 then
-  echo
-  echo "--> Downloading Fiji"
-  curl -fsO https://downloads.imagej.net/fiji/latest/fiji-nojre.zip
+	echo
+	echo "--> Downloading Fiji"
+	curl -fsO https://downloads.imagej.net/fiji/latest/fiji-nojre.zip
 
-  echo "--> Unpacking Fiji"
-  rm -rf Fiji.app
-  unzip fiji-nojre.zip
+	echo "--> Unpacking Fiji"
+	rm -rf Fiji.app
+	unzip fiji-nojre.zip
 fi
 
 # -- Determine correct ImageJ launcher executable --
 case "$(uname -s),$(uname -m)" in
-  Linux,x86_64) launcher=ImageJ-linux64 ;;
-  Linux,*) launcher=ImageJ-linux32 ;;
-  Darwin,*) launcher=Contents/MacOS/ImageJ-macosx ;;
-  MING*,*) launcher=ImageJ-win32.exe ;;
-  *) die "Unknown platform" ;;
+	Linux,x86_64) launcher=ImageJ-linux64 ;;
+	Linux,*) launcher=ImageJ-linux32 ;;
+	Darwin,*) launcher=Contents/MacOS/ImageJ-macosx ;;
+	MING*,*) launcher=ImageJ-win32.exe ;;
+	*) die "Unknown platform" ;;
 esac
 
 echo
