a316b93f5855713670624d5b5f640f4d75046da3
Curtis Rueden <ctrueden@wisc.edu>
2019-11-01 15:16:20 -0500

    Release version 0.5.0

--- imagej/imagej.py
+++ imagej/imagej.py
@@ -5,7 +5,7 @@ wrapper for imagej and python integration using ImgLyb
 
 # TODO: Unify version declaration to one place.
 # https://www.python.org/dev/peps/pep-0396/#deriving
-__version__ = '0.4.1.dev0'
+__version__ = '0.5.0'
 __author__ = 'Curtis Rueden, Yang Liu, Michael Pinkert'
 
 import os, re
