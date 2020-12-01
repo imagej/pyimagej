bf281a8915beff3bf0494cf9ea5cee848f92a0d7
Curtis Rueden <ctrueden@wisc.edu>
2020-11-30 21:55:48 -0600

    Clean up the debugtools a bit

--- debugtools.py
+++ debugtools.py
@@ -1,8 +1,8 @@
 import jpype.imports
-import scyjava.config
+import scyjava
+
 
 def print_classpath():
-
     from java.lang import System
 
     # get classpath and split
@@ -14,33 +14,23 @@ def print_classpath():
     for i in x:
         print(i)
 
-    return
 
 def print_endpoints():
-
     endpoints = scyjava.config.get_endpoints()
     print("[DEBUG] endpoints: {0}".format(endpoints))
 
-    return
 
 def print_ij_version(ij):
-
     print("[DEBUG] ij version: {0}".format(ij.getVersion()))
 
-    return
 
 def print_obj_dir(object):
-
     obj_dir = dir(object)
 
     print("[DEBUG] object name: {0}\n[DEBUG] dir:".format(str(object)))
     for x in obj_dir:
         print(x)
 
-    return
 
 def print_obj_type(object):
-
     print("[DEBUG] object name: {0}\n[DEBUG] type: {1}".format(str(object), type(object)))
-
-    return
