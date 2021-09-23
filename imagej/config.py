fce6d1665c02f2f0fd75e84c30cf32cb36065eaa
hinerm <hinerm@gmail.com>
2021-09-23 13:02:36 -0500

    Release version 1.0.2

--- imagej/config.py
+++ imagej/config.py
@@ -3,5 +3,5 @@ __author__  = 'Curtis Rueden, Michael Pinkert, Ed Evans, Yang Liu, Mark Hiner'
 __major__   = 1
 __minor__   = 0
 __patch__   = 2
-__tag__     = 'dev0'
+__tag__     = ''
 __version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
