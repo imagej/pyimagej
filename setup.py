da9330a1ab69d6877b95dcef8fee8915eeab6e34
Edward Evans <elevans2@wisc.edu>
2021-10-28 11:42:16 -0500

    Fix pypi readme links to github pages

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md')) as f:
-    pyimagej_long_description = f.read()
+    readme = f.read()\
+        .replace('](doc', '](https://github.com/imagej/pyimagej/blob/master/doc')\
+        .replace('](issues', '](https://github.com/imagej/pyimagej/issues')
 
 config={}
 with open('imagej/config.py', 'r') as f:
@@ -19,7 +21,7 @@ setup(
     packages=find_packages(),
     platforms=['any'],
     description='Python wrapper for ImageJ',
-    long_description=pyimagej_long_description,
+    long_description=readme,
     long_description_content_type='text/markdown',
     license='Apache 2.0',
     install_requires=[
