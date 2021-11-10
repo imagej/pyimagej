549b670f7191105401bf30436695906b7be2f8b0
Edward Evans <elevans2@wisc.edu>
2021-11-10 14:28:25 -0600

    Use net.imglib.util.Interval to get dims
    This returns the dimension order that Interval sees
    which is import for data slicing with net.imglib2.view.Views.

--- imagej/__init__.py
+++ imagej/__init__.py
@@ -230,8 +230,8 @@ def init(ij_dir_or_version_or_endpoint=None, headless=True):
                 return image.shape
             if not sj.isjava(image):
                 raise TypeError('Unsupported type: ' + str(type(image)))
-            if sj.jclass('net.imglib2.Dimensions').isInstance(image):
-                return [image.dimension(d) for d in range(image.numDimensions() -1, -1, -1)]
+            if isinstance(image, RandomAccessibleInterval):
+                return tuple(sj.jimport('net.imglib2.util.Intervals').dimensionsAsLongArray(image))
             if sj.jclass('ij.ImagePlus').isInstance(image):
                 dims = image.getDimensions()
                 dims.reverse()
