9d8ff2ab4242f9b063f7e143244e42ab811d5d2c
Edward Evans <elevans2@wisc.edu>
2021-12-23 09:31:52 -0600

    Add headless blob detection example and test image
    This example is intended to run with xvfb in order
    to achieve headless RoiManager and ResultsTable
    interactions.

---
+++ doc/examples/headless_blob_detection.py
@@ -0,0 +1,90 @@
+import imagej
+import xarray as xr
+import numpy as np
+import scyjava as sj
+import code # remove after debugging
+
+from math import sqrt
+from skimage.feature import blob_log
+from matplotlib import pyplot as plt
+
+def find_blobs(image: xr.DataArray, min_sigma: float, max_sigma: float, num_sigma: int, threshold=0.1) -> np.ndarray:
+    """
+    Find blobs with Laplacian of Gaussian (LoG).
+    """
+    # if xarray.DataArray extract the numpy array
+    if isinstance(image, xr.DataArray):
+        image = image.squeeze()
+        image = image.data
+
+    # detect blobs in image
+    blobs = blob_log(image, min_sigma=min_sigma, max_sigma=max_sigma, num_sigma=num_sigma, threshold=threshold)
+    blobs[:, 2] = blobs[:, 2] * sqrt(2)
+
+    return blobs
+
+
+def process(image, oval_array: np.ndarray, add_to_roi_manager=True, multimeasure=True):
+    """
+    Process the blob rois.
+    """
+    # get ImageJ resoures
+    ImagePlus = sj.jimport('ij.ImagePlus')
+    OvalRoi = sj.jimport('ij.gui.OvalRoi')
+    Overlay = sj.jimport('ij.gui.Overlay')
+    ov = Overlay()
+
+    # convert image to imp
+    imp = ij.convert().convert(image, ImagePlus)
+
+    if add_to_roi_manager:
+        rm = ij.RoiManager().getRoiManager()
+
+    for i in range(len(oval_array)):
+        values = oval_array[i].tolist()
+        y = values[0]
+        x = values[1]
+        r = values[2]
+        d = r * 2
+        roi = OvalRoi(x - r, y - r, d, d)
+        imp.setRoi(roi)
+        ov.add(roi)
+        if add_to_roi_manager:
+            rm.addRoi(roi)
+    
+    imp.setOverlay(ov)
+    imp.show()
+
+    if rm != None and multimeasure:
+       # TODO: Bug -- doesn't perform multimeasure properly
+       indexes = rm.getIndexes()
+       rm.setSelectedIndexes(indexes)
+       print(f"Selected roi: {rm.getSelectedIndexes()}")
+       return rm.multiMeasure(imp)
+    
+    return None
+
+
+def get_dataframe(table):
+    """
+    Convert results table to pandas Dataframe.
+    """
+    Table = sj.jimport('org.scijava.table.Table')
+    sci_table = ij.convert().convert(table, Table)
+
+    return ij.py.from_java(sci_table)
+
+
+if __name__ == "__main__":
+    # initialize imagej
+    ij = imagej.init(mode='interactive')
+    print(f"ImageJ version: {ij.getVersion()}")
+
+    # load sample data and run blob detection
+    img = ij.io().open('../sample-data/test_image.tif')
+    img_xr = ij.py.from_java(img)
+    detected_blobs = find_blobs(img_xr, min_sigma=0.5, max_sigma=3, num_sigma=10, threshold=0.0075)
+    results_table = process(img, detected_blobs)
+    df = get_dataframe(results_table)
+    print(df)
+    code.interact(local=locals())
