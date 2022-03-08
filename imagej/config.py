4c6d1c2c7e39b902638f8582f13730ba977d1ea9
Edward Evans <elevans2@wisc.edu>
2022-03-08 14:47:26 -0600

    Bump to next development cycle

--- imagej/config.py
+++ imagej/config.py
@@ -2,6 +2,6 @@ __author__  = 'Curtis Rueden, Michael Pinkert, Ed Evans, Yang Liu, Mark Hiner'
 
 __major__   = 1
 __minor__   = 1
-__patch__   = 0
-__tag__     = ''
+__patch__   = 1
+__tag__     = 'dev0'
 __version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
