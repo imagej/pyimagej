1145321594bfa4463fab2e61882c582c77cd2a18
Edward Evans <elevans2@wisc.edu>
2022-03-10 09:27:26 -0600

    Release version 1.1.1

--- imagej/config.py
+++ imagej/config.py
@@ -3,5 +3,5 @@ __author__  = 'Curtis Rueden, Michael Pinkert, Ed Evans, Yang Liu, Mark Hiner'
 __major__   = 1
 __minor__   = 1
 __patch__   = 1
-__tag__     = 'dev0'
+__tag__     = ''
 __version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
