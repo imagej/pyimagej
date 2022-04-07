62219c801918be575cd74eeed87946b576944c92
Curtis Rueden <ctrueden@wisc.edu>
2022-04-07 15:22:38 -0500

    test.sh: only run tests in test folder
    The Python sources in doc are not for you, pytest!

--- test.sh
+++ test.sh
@@ -6,16 +6,16 @@ cd "$dir"
 echo "-------------------------------------"
 echo "| Testing ImageJ2 + original ImageJ |"
 echo "-------------------------------------"
-pytest -p no:faulthandler
+pytest -p no:faulthandler test
 
 echo
 echo "-------------------------------------"
 echo "|    Testing ImageJ2 standalone     |"
 echo "-------------------------------------"
-pytest -p no:faulthandler --legacy=false
+pytest -p no:faulthandler --legacy=false test
 
 echo
 echo "-------------------------------------"
 echo "|  Testing Fiji Is Just ImageJ(2)   |"
 echo "-------------------------------------"
-pytest -p no:faulthandler --ij=sc.fiji:fiji
+pytest -p no:faulthandler --ij=sc.fiji:fiji test
