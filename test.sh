6ddd5428a366504e19660f2346b9b8a53fc98ce5
Curtis Rueden <ctrueden@wisc.edu>
2022-04-14 12:12:40 -0500

    Make pytest execution more robust
    We want to be sure to use the same installed pytest package from the
    current python environment, rather than whatever the pytest executable
    happens to be pointing to (which could be different than python).
    
    With this change, we can also simplify the CI build to use test.sh.

--- test.sh
+++ test.sh
@@ -6,16 +6,16 @@ cd "$dir"
 echo "-------------------------------------"
 echo "| Testing ImageJ2 + original ImageJ |"
 echo "-------------------------------------"
-pytest -p no:faulthandler test
+python -m pytest -p no:faulthandler test
 
 echo
 echo "-------------------------------------"
 echo "|    Testing ImageJ2 standalone     |"
 echo "-------------------------------------"
-pytest -p no:faulthandler --legacy=false test
+python -m pytest -p no:faulthandler --legacy=false test
 
 echo
 echo "-------------------------------------"
 echo "|  Testing Fiji Is Just ImageJ(2)   |"
 echo "-------------------------------------"
-pytest -p no:faulthandler --ij=sc.fiji:fiji test
+python -m pytest -p no:faulthandler --ij=sc.fiji:fiji test
