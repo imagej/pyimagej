7244a01cbcd0b82bb0d5464e0262b3df49744741
Curtis Rueden <ctrueden@wisc.edu>
2022-04-29 06:13:11 -0500

    Bump to next development cycle

--- src/imagej/config.py
+++ src/imagej/config.py
@@ -2,6 +2,6 @@ __author__ = "Curtis Rueden, Ed Evans, Mark Hiner, Michael Pinkert, Gabe Selzer,
 
 __major__ = 1
 __minor__ = 2
-__patch__ = 1
-__tag__ = ""
+__patch__ = 2
+__tag__ = "dev0"
 __version__ = f"{__major__}.{__minor__}.{__patch__}.{__tag__}".strip(".")
