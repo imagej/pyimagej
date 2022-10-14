6f1b2095a9a2868372be0bcbde12145a1da817ff
Curtis Rueden <ctrueden@wisc.edu>
2022-10-14 10:40:10 -0500

    bin/clean.sh: report what gets removed

--- bin/clean.sh
+++ bin/clean.sh
@@ -4,6 +4,6 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 find . -name __pycache__ -type d | while read d
-  do rm -rf "$d"
+  do rm -rfv "$d"
 done
-rm -rf .pytest_cache build dist doc/_build doc/rtd src/*.egg-info
+rm -rfv .pytest_cache build dist doc/_build doc/rtd src/*.egg-info
