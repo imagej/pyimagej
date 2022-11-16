c789fd12d057c2fafeea2f362ab103e1f406219a
Curtis Rueden <ctrueden@wisc.edu>
2022-11-15 18:43:12 -0600

    Remove third line of SLF4J warning
    It got indented one more level, which pushed
    it over the max line length limit for flake8.

--- src/imagej/__init__.py
+++ src/imagej/__init__.py
@@ -1394,7 +1394,6 @@ def _create_jvm(
         # Add SciJava logging configuration. Without this, we see the warning:
         # SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
         # SLF4J: Defaulting to no-operation (NOP) logger implementation
-        # SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
         sj.config.endpoints.append("org.scijava:scijava-config:MANAGED")
 
     # Add additional ImageJ endpoints specific to PyImageJ.
