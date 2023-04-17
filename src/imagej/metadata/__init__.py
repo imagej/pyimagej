8af2bdcb07f12feb5937de5cb13fc4b7a5b6ff7e
Edward Evans <elevans2@wisc.edu>
2023-04-17 14:41:34 -0500

    Refactor create imagej metadata function

--- src/imagej/metadata/__init__.py
+++ src/imagej/metadata/__init__.py
@@ -1,37 +1,34 @@
 from typing import Sequence
 
-import imagej.dims as dims
 import imagej.metadata.axis as axis
 from imagej._java import jc
 
 
-def create_imagej_metadata(
-    axes: Sequence["jc.CalibratedAxis"], dim_seq: Sequence[str]
-) -> dict:
-    """
-    Create the ImageJ metadata attribute dictionary for xarray's global attributes.
-    :param axes: A list or tuple of ImageJ2 axis objects
-        (e.g. net.imagej.axis.DefaultLinearAxis).
-    :param dim_seq: A list or tuple of the dimension order (e.g. ['X', 'Y', 'C']).
-    :return: Dict of image metadata.
-    """
+def _create_axis_metadata(img: "jc.ImgPlus") -> Sequence[dict]:
+    meta_arr = []
+    axes = list(img.dim_axes)[::-1]
+    dims = list(img.dims)[::-1]
+    shape = list(img.shape)[::-1]
+
+    for i in range(len(dims)):
+        ax = axes[i]
+        ax_meta = {}
+        # get per axis metadata
+        ax_meta["label"] = dims[i]
+        ax_meta["length"] = shape[i]
+        ax_meta["CalibratedAxis"] = axis.calibrated_axis_to_str(ax)
+        if hasattr(ax, "scale"):
+            ax_meta["scale"] = float(ax.scale())
+        if hasattr(ax, "origin"):
+            ax_meta["origin"] = float(ax.origin())
+        # store metadata
+        meta_arr.append(ax_meta)
+
+    return meta_arr
+
+
+def create_imagej_metadata(img: "jc.ImgPlus"):
     ij_metadata = {}
-    if len(axes) != len(dim_seq):
-        raise ValueError(
-            f"Axes length ({len(axes)}) does not match \
-                dimension length ({len(dim_seq)})."
-        )
-
-    for i in range(len(axes)):
-        # get CalibratedAxis type as string (e.g. "EnumeratedAxis")
-        ij_metadata[
-            dims._to_ijdim(dim_seq[i]) + "_cal_axis_type"
-        ] = axis.calibrated_axis_to_str(axes[i])
-        # get scale and origin for DefaultLinearAxis
-        if isinstance(axes[i], jc.DefaultLinearAxis):
-            ij_metadata[dims._to_ijdim(dim_seq[i]) + "_scale"] = float(axes[i].scale())
-            ij_metadata[dims._to_ijdim(dim_seq[i]) + "_origin"] = float(
-                axes[i].origin()
-            )
-
+    # imagej metadata sections
+    ij_metadata["axis"] = _create_axis_metadata(img)
     return ij_metadata
