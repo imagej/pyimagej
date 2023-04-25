615ee25c37b198527ce72024a6f18c5d5c6ec923
Edward Evans <elevans2@wisc.edu>
2023-04-25 14:03:32 -0500

    Add type hint to create_imagej_metadata function

--- src/imagej/metadata/__init__.py
+++ src/imagej/metadata/__init__.py
@@ -27,8 +27,9 @@ def _create_axis_metadata(img: "jc.ImgPlus") -> Sequence[dict]:
     return meta_arr
 
 
-def create_imagej_metadata(img: "jc.ImgPlus"):
+def create_imagej_metadata(img: "jc.ImgPlus") -> dict:
     ij_metadata = {}
     # imagej metadata sections
     ij_metadata["axis"] = _create_axis_metadata(img)
+
     return ij_metadata
