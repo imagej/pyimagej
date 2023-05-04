8baaac0376d19bb2fc3440b4a7ced65402bb4b07
Curtis Rueden <ctrueden@wisc.edu>
2023-05-04 13:27:07 -0500

    lint.sh: exit non-zero if any step fails

--- bin/lint.sh
+++ bin/lint.sh
@@ -3,7 +3,13 @@
 dir=$(dirname "$0")
 cd "$dir/.."
 
+exitCode=0
 black src tests
+code=$?; test $code -eq 0 || exitCode=$code
 isort src tests
+code=$?; test $code -eq 0 || exitCode=$code
 python -m flake8 src tests
+code=$?; test $code -eq 0 || exitCode=$code
 validate-pyproject pyproject.toml
+code=$?; test $code -eq 0 || exitCode=$code
+exit $exitCode
