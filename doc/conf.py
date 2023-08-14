bb4c6195f2fb2be607ab746c211dbf755b1a3233
Edward Evans <elevans2@wisc.edu>
2023-08-14 09:38:24 -0500

    Add PyImageJ logo to read the docs

--- doc/conf.py
+++ doc/conf.py
@@ -13,14 +13,14 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../src/imagej/'))
+sys.path.insert(0, os.path.abspath("../src/imagej/"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PyImageJ'
-copyright = '2022 ImageJ2 developers'
-author = 'ImageJ2 developers'
+project = "PyImageJ"
+copyright = "2022 ImageJ2 developers"
+author = "ImageJ2 developers"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,32 +29,45 @@ author = 'ImageJ2 developers'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx_search.extension',
-    'myst_nb',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_search.extension",
+    "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md', 'examples/README.md']
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "README.md",
+    "examples/README.md",
+]
 
-# -- MyST-Parser/MyST-NB configuration ------------------------------------------------------
+# -- MyST-Parser/MyST-NB configuration ---------------------------------------
 myst_heading_anchors = 4
-nb_execution_mode = 'off'
+nb_execution_mode = "off"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# Add the PyImageJ logo
+html_logo = "doc-images/logo.svg"
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
