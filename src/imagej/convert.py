a1861b6c1658d6751fa314650b13411f956549ab
hinerm <hinerm@gmail.com>
2023-09-21 15:42:53 -0500

    Ensure converted DataArrays have pythonic names
    Without this, they will have jpype Strings, which do not always behave
    in a pythonic way (e.g. are not pickleable).

--- src/imagej/convert.py
+++ src/imagej/convert.py
@@ -237,6 +237,7 @@ def java_to_xarray(ij: "jc.ImageJ", jobj) -> xr.DataArray:
     xr_dims = dims._convert_dims(xr_dims, direction="python")
     xr_coords = dims._get_axes_coords(xr_axes, xr_dims, narr.shape)
     name = jobj.getName() if isinstance(jobj, jc.Named) else None
+    name = ij.py.from_java(name)
     return xr.DataArray(narr, dims=xr_dims, coords=xr_coords, attrs=xr_attrs, name=name)
 
 
