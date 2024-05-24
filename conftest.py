3bae68b9201896460ef4d134a1501765952c63f3
Edward Evans <elevans2@wisc.edu>
2024-05-24 15:05:14 -0500

    Add docs section on how to use when_imagej_starts
    This commit adds section 5.2 to the "Convenience methods of
    PyImageJ" that describes how to use the when_image_starts
    callback mechanism.

--- conftest.py
+++ conftest.py
@@ -43,7 +43,9 @@ def ij(request):
     legacy = request.config.getoption("--legacy")
     headless = request.config.getoption("--headless")
 
-    imagej.when_imagej_starts(lambda ij: setattr(ij, "_when_imagej_starts_result", "success"))
+    imagej.when_imagej_starts(
+        lambda ij: setattr(ij, "_when_imagej_starts_result", "success")
+    )
 
     mode = "headless" if headless else "interactive"
     ij = imagej.init(ij_dir, mode=mode, add_legacy=legacy)
