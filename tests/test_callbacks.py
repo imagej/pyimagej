b2ce4cb21836bacc68338c6b2dc84fc879e081fd
Edward Evans <elevans2@wisc.edu>
2024-10-14 09:55:53 -0500

    Use None as the getattr default, not True
    The default here should be None, not True, otherwise
    this test does not actually test for the `_testing`
    attribute value.

--- tests/test_callbacks.py
+++ tests/test_callbacks.py
@@ -4,4 +4,4 @@ def test_when_imagej_starts(ij):
     when_imagej_starts, which injects a small piece of data into the gateway
     object. We check for that data here to make sure the callback happened.
     """
-    assert True is getattr(ij, "_testing", True)
+    assert True is getattr(ij, "_testing", None)
